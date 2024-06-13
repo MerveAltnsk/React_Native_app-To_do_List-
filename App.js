e85f68a5c6bbcac0abdde94c5acff768bd907146
merve <merve@MERVEA>
2024-06-13 14:48:29 +0300

    Last Changes

--- App.js
+++ App.js
@@ -1,47 +1,72 @@
 import React, {useState} from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, View, TextInput, TouchableOpacity } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView,ImageBackground } from 'react-native';
 import Task from './components/Task';
 
+
 export default function App() {
-  const [text, setText] = useState();
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
 
   const handleAddTask = () => {
-    console.log(task);
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
   }
 
   return (
     <View style={styles.container}>
-     {/* Today's Tasks */}
-     <View style={styles.taskWrapper}>
-       <Text style={styles.sectionTitle}>Today's Tasks</Text>
-
-       <View style={styles.items}>
-        {/* This is where the tasks will go */}
-        <Task text ={'Task 1'} />
-        <Task text ={'Task 2'} />
-       </View>
-
-     </View>
-
-
-
-
-     {/* Write a Tasks */}
-     <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.writeTaskWrapper}
+      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+      <ImageBackground
+        source={require('./img/b1.png')}
+        style={styles.imageBackground}
       >
-        <TextInput style ={styles.input} placeholder={'Write a Task'}/>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
 
-        <TouchableOpacity>
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                  <Task text={item} /> 
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+        
+      </ScrollView>
+
+      {/* Write a task */}
+      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
-
           </View>
         </TouchableOpacity>
-
-     </KeyboardAvoidingView>
-
+      </KeyboardAvoidingView>
+    </ImageBackground>
+      
     </View>
   );
 }
@@ -49,53 +74,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEAED',
   },
-
-  taskWrapper: {
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  tasksWrapper: {
     paddingTop: 80,
-    paddingHorizontal:20,
+    paddingHorizontal: 20,
   },
-
   sectionTitle: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#000000', 
   },
-
   items: {
     marginTop: 30,
   },
-
-
-  writeTaskWrapper:{
+  writeTaskWrapper: {
     position: 'absolute',
     bottom: 60,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
     width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   },
   input: {
-    paddingHorizontal: 15,
     paddingVertical: 15,
-    borderWidth: 1,
-    borderColor: '#C0C0C0',
-    borderRadius: 100,
-    width: 60,
+    paddingHorizontal: 15,
     backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
   },
   addWrapper: {
     width: 60,
     height: 60,
     backgroundColor: '#FFF',
     borderRadius: 60,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
     borderWidth: 1,
-    borderColor: '#C0C0C0'
   },
-  addText: {
-
-  },
-
-});
+  addText: {},
+}); 
